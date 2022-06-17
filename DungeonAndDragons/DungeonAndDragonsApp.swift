e04c3cdc35af990d6583c13df62e1ceb4cbeafbf
Chris Warner <chris@codebythesea.com>
2022-06-17 09:29:03 -0700

    Initial Commit

---
+++ DungeonAndDragons/DungeonAndDragonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonAndDragonsApp.swift
+//  DungeonAndDragons
+//
+//  Created by chris warner on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct DungeonAndDragonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
